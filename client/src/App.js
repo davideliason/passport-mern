f4697e31410d75e574de98456b21fd1fd549195f
David Eliason <davethemaker@gmail.com>
2018-10-17 11:18:23 -0700

    import and use Link to /dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import SignupForm from './components/sign-up.js'
+import { Route, Link } from 'react-router-dom'
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+         <nav>
+           <Link to="/dashboard">Dashboard</Link>
+         </nav>
         <SignupForm />
       </div>
     );
