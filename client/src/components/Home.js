c58028df7a43ca37931bd710a21fc6d6ee435e9d
David Eliason <davethemaker@gmail.com>
2018-10-17 14:11:01 -0700

    update rendered name in welcome format for Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,14 +4,14 @@ const Home = props => {
 	if (props.user) {
 		return (
 			<div className="Home">
-				<p>Current User:</p>
-				{props.user.name}
+				<p>Welcome Home, {props.user.name}</p>
+				
 			</div>
 		)
 	} else {
 		return (
 			<div className="Home">
-				<p>Current User:</p>
+				<p>Hello Stranger!:</p>
 				<code>
 					{JSON.stringify(props)}
 				</code>
