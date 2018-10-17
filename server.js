868060701abd2e49e39c891c5c039e25c338b5d6
David Eliason <davethemaker@gmail.com>
2018-10-17 15:02:17 -0700

    add body-parser which now allows us in server.js to log out signUpForm form input field data as a result of axios POST upon form submission

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.get('/', (req,res) => {
     res.end("hello world");
@@ -8,6 +15,9 @@ app.get('/', (req,res) => {
 
 app.post('/auth/signup',(req,res) => {
     console.log("POST");
+    console.log(req.body.username)
+    console.log(req.body.password)
+
 });
 
 app.listen(port, ()=>{
