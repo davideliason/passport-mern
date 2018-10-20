64a8eaf366ee2bc39a0870db92987cc85d943a0a
David Eliason <davethemaker@gmail.com>
2018-10-20 16:47:43 -0700

    add Signupform component with username and password field inputs and click handlers to set state values and finally POST a request to server

---
+++ client/components/sign-up.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Signup extends Component {
+	constructor() {
+		super()
+		this.state = {
+			username: '',
+			password: ''
+		}
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+		this.handlePasswordChange = this.handlePasswordChange.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+	handleUsernameChange(event) {
+		this.setState({
+			username: event.target.value
+		})
+    }
+    
+    handlePasswordChange(event) {
+		this.setState({
+			password: event.target.value
+		})
+    }
+    
+	handleSubmit(event) {
+		console.log('sign up form submit click handler invoked');
+        console.log(this.state.username)
+        console.log(this.state.password)
+		event.preventDefault()
+
+        //post to server to add a new usernamr and password
+        // from state values
+		axios.post('/user/', {
+			username: this.state.username,
+			password: this.state.password
+		})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg) {
+					console.log('successful signup')
+					this.setState({ //redirect to login page
+						redirectTo: '/login'
+					})
+				} else {
+					console.log('username already taken')
+				}
+			}).catch(error => {
+				console.log('signup error: ')
+				console.log(error)
+
+			})
+	}
+
+
+render() {
+	return (
+		<div className="SignupForm">
+			<h4>Sign up</h4>
+			<form className="form-horizontal">
+				<div className="form-group">
+					<div className="col-1 col-ml-auto">
+						<label>>Username</label>
+					</div>
+					<div className="col-3 col-mr-auto">
+						<input className="form-input"
+							type="text"
+							id="username"
+							name="username"
+							placeholder="Username"
+							value={this.state.username}
+							onChange={this.handleUsernameChange}
+						/>
+					</div>
+				</div>
+				<div className="form-group">
+					<div className="col-1 col-ml-auto">
+						<label> Password: </label>
+					</div>
+					<div className="col-3 col-mr-auto">
+						<input className="form-input"
+							placeholder="password"
+							type="password"
+							name="password"
+							value={this.state.password}
+							onChange={this.handlePasswordChange}
+						/>
+					</div>
+				</div>
+				<div className="form-group ">
+					<div className="col-7"></div>
+					<button
+						className="btn btn-primary col-1 col-mr-auto"
+						onClick={this.handleSubmit}
+						type="submit"
+					>Sign up</button>
+				</div>
+			</form>
+		</div>
+
+	)
+}
+}
+
+export default Signup
